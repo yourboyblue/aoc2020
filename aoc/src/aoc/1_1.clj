d93355733ee488791bc391a1401fae594a610798
Benjamin Blue <bencblue@gmail.com>
2020-12-02 00:50:05 -0800

    Solution for day 1 part 1
    This is less efficient than it could be since it doesn't break when the
    matching pair is found. Still figuring out the recursion.

--- aoc/src/aoc/1_1.clj
+++ aoc/src/aoc/1_1.clj
@@ -1,6 +1,23 @@
-(ns aoc.1-1)
+(ns aoc.1-1
+  (:require [clojure.string :as string])
+  (:require [aoc.utils :as utils]))
 
-(defn run
-  "Day one part one solver"
-  [input]
-  (print input))
+;; process input to sorted list of integers
+(defn to-int [n] (Integer/parseInt n))
+(defn process-input [input] (sort (map to-int (string/split (string/trim input) #"\n"))))
+
+(def input (process-input (utils/input 1)))
+
+;; this could be more efficient since it doesn't stop when it finds the pair
+(defn find-pair [numbers]
+  (first
+   (for [a (reverse numbers)
+         b numbers
+         :let [c (+ a b)]
+         :while (>= 2020 c)
+         :when (= 2020 c)]
+     [a, b])))
+
+(defn run []
+  (let [[a b] (find-pair input)]
+    (* a b) ))
